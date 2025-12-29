dd919d954998b58c2a3b9bd58ed644fb23e7b628
Sonali-Patel98 <sonalipatel9845@gmail.com>
2025-12-29 23:50:17 +0530

    trip basic info form

---
+++ src/constants/options.jsx
@@ -0,0 +1,53 @@
+export const SelectTravelesList=[
+    {
+        id:1,
+        title:'Just Me',
+        desc:'A sole traveles in exploration',
+        icon:'✈️',
+        people:'1'
+    },
+    {
+        id:2,
+        title:'A Couple',
+        desc:'Two traveles in tandem',
+        icon:'',
+        people:'2 People'
+    },
+    {
+        id:3,
+        title:'Family',
+        desc:'A group of fun loving adv',
+        icon:'',
+        people:'3 to 5 People'
+    },
+    {
+        id:4,
+        title:'Friends',
+        desc:'A bunch of thrill-seekes',
+        icon:'',
+        people:'5 to 10 People'
+    },
+]
+
+export const SelectBudgetOptions=[
+    {
+        id:1,
+        title:'cheap',
+        desc:'Stay conscious of costs',
+        icon:'',
+    },
+    {
+        id:2,
+        title:'Moderate',
+        desc:'keep cost on the average side',
+        icon:'',
+    },
+    {
+        id:3,
+        title:'Luxury',
+        desc:'dont worry about cost',
+        icon:'',
+    },
+]
+
+export const AI_PROMPT='Generate Travel Plan for Location : {location}'
