187e22877d7f279fefdada576ff3ca627caa7bef
Sonali-Patel98 <sonalipatel9845@gmail.com>
2026-01-02 17:34:01 +0530

    trip

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect,useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import { Input } from "@/components/ui/input";
 import { SelectBudgetOptions, SelectTravelesList } from '@/constants/options';
@@ -6,6 +6,27 @@ import { Button } from '@/components/ui/button';
 
 function CreateTrip() {
   const [place, setPlace] = useState();
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+   useEffect(() => {
+    if (query.length < 3) {
+      setResults([]);
+      return;
+    }
+
+    const timer = setTimeout(async () => {
+      const res = await fetch(
+        `https://nominatim.openstreetmap.org/search?format=json&q=${query}`
+      );
+      const data = await res.json();
+      setResults(data);
+      console.log("options:",data);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [query]);
+
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10 relative left-[14rem]'>
       <h2 className='font-bold text-3xl'>Tell us Your travel preferences 🏕️🌴</h2>
@@ -15,12 +36,41 @@ function CreateTrip() {
       <div className='mt-20 flex flex-col gap-10'>
         <div>
           <h2 className='text-xl my-3 font-medium'>What is destination of choice ?</h2>
-          <GooglePlacesAutocomplete
+          {/* <GooglePlacesAutocomplete
             apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
             selectProps={{
               place, onChange: (v) => { setPlace(v); console.log(v) }
             }}
+          /> */}
+
+          <input type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search destination..."
+          className="w-full p-3 border rounded-md outline-blue-500"
           />
+
+          {results.length > 0 && (
+          <ul className="absolute w-full bg-white border rounded-md mt-1 max-h-52 overflow-y-auto z-10">
+            {results.map((place) => (
+              <li
+                key={place.place_id}
+                onClick={() => {
+                setQuery(place.display_name);
+                setResults([]);
+                console.log(place); // full address + lat + lon
+                }}
+                className="p-3 cursor-pointer hover:bg-gray-100"
+              >
+              {place.display_name}
+              </li>
+            ))}
+          </ul>
+          )}
+
+
+
+
         </div>
         <div >
           <h2 className='text-xl my-3 font-medium'>How many days are you planning your trip ?</h2>
@@ -54,12 +104,7 @@ function CreateTrip() {
       <div className='my-10 flex justify-end'>
         <Button className='box text-white'>Generate Trip</Button>
       </div>
-      <div>
-        <div>hii</div>
-      </div>
-      <div>
-        <div>hii</div>
-      </div>
+      
       
     </div>
     
